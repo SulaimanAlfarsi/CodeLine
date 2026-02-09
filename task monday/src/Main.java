9b7eeadbdaf0f77d57a520c7a74d4adf810879c8
SulaimanAlfarsi <sulaimanalfarsi3060@gmail.com>
2026-02-09 11:01:17 +0400

    Initial commit: add tasks folders

---
+++ task monday/src/Main.java
@@ -0,0 +1,55 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Pathfinder: The Windows File Trail
+        System.out.println("C:\\Users\\Student\\Documents\\Java\\Main.java\n");
+
+        // Welcome, Wagon: The Three-Line Greeting
+
+        System.out.println("Welcome to Java Programming\nLearn, Practice, Succeed\nHappy Coding! \n");
+
+
+        // Code in Quotes: cc
+
+        System.out.println("Path: \"C:\\Program Files\\Java\"");
+
+        // Boxed Brilliance: The Asterisk Frame
+
+        int width = 5;
+
+        System.out.println("*".repeat(width));
+        System.out.println("*" + " ".repeat(width - 2) + "*");
+        System.out.println("*" + " ".repeat(width - 2) + "*");
+        System.out.println("*".repeat(width));
+
+        // Directory Detective: The Tree Reveal
+
+        System.out.println("Root\\");
+        System.out.println("\tDocuments\\");
+        System.out.println("\t\tresume.pdf");
+        System.out.println("\t\tletter.doc");
+        System.out.println("\tPhotos\\");
+        System.out.println("\t\tvacation.jpg");
+
+        // Talk the Talk: Quoted Dialogues
+
+        System.out.println("Teacher: \"What is your name?\"");
+        System.out.println("Student: \"My name is Alex.\"");
+        System.out.println("Teacher: \"Welcome, Alex!\"");
+
+
+        // Pyramid Power: The Star Mountain
+
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+
+        
+
+
+    }
+}
---
+++ tasks sunday/src/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //task 1
+
+        String separator = "-".repeat(20);
+
+        System.out.println("Section 1: Introduction");
+        System.out.println(separator);
+
+        System.out.println("Section 2: Methods");
+        System.out.println(separator);
+
+        System.out.println("Section 3: Conclusion");
+        System.out.println(separator);
+
+
+
+        //task 2
+        System.out.println("#".repeat(1));
+        System.out.println("#".repeat(2));
+        System.out.println("#".repeat(3));
+        System.out.println("#".repeat(4));
+        System.out.println("#".repeat(5));
+
+
+
+        //task 3
+
+        System.out.println("=".repeat(50));
+
+
+        //task 4
+        String text = "Keep Learning";
+
+        System.out.println("*".repeat(20));
+        System.out.println("*                  *");
+        System.out.println(text);
+        System.out.println("*                  *");
+        System.out.println("*".repeat(20));
+
+
+
+        //task 5
+
+        System.out.println("* ".repeat(5));
+        System.out.println("* ".repeat(5));
+        System.out.println("* ".repeat(5));
+        System.out.println("* ".repeat(5));
+        System.out.println("* ".repeat(5));
+
+
+
+
+    }
+}
